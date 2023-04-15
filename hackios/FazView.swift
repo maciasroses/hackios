b55046c31914ea03202e05370055f30e6ead668a
fazfaz7 <adrian.faz123@gmail.com>
2023-04-15 12:47:23 -0600

    Prueba FazView

---
+++ hackios/FazView.swift
@@ -0,0 +1,20 @@
+//
+//  FazView.swift
+//  hackios
+//
+//  Created by Adrian Faz on 15/04/23.
+//
+
+import SwiftUI
+
+struct FazView: View {
+    var body: some View {
+       Text("Prueba FazView")
+    }
+}
+
+struct FazView_Previews: PreviewProvider {
+    static var previews: some View {
+        FazView()
+    }
+}
