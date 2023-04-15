608e79a2155b07c3415603198426c40a966b8d36
Macias Romero Jorge Humberto <a00573134@itesm.mx>
2023-04-15 13:25:23 -0600

    Primera visualizacion de la segunda pantalla

--- hackios/ContentView.swift
+++ hackios/ContentView.swift
@@ -9,13 +9,64 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack{
+            HStack {
+                VStack{
+                    Text("Title1")
+                    ForEach(0..<3, id:  \.self) { number in
+                        HStack{
+                            Button(action: {
+                                print("Holi")
+                            }, label: {
+                                Image(systemName: "speaker.wave.2.fill")
+                                    .font(.system(size: 25,weight: .regular, design: .default) )
+                            }
+                            )
+                            Button(action: {
+                                print("Holi2")
+                            }, label: {
+                                Image(systemName: "speaker.wave.2.fill")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width:25)
+                                    .padding(.horizontal,5)
+                            Text(  "Ejemplo").foregroundColor(.black).fontWeight(.semibold)
+                            }
+                            )
+                        }
+                    }
+                }
+                .padding()
+                
+                VStack{
+                    Text("Title2")
+                    ForEach(0..<3, id:  \.self) { number in
+                        HStack{
+                            Button(action: {
+                                print("Holi")
+                            }, label: {
+                                Image(systemName: "speaker.wave.2.fill")
+                                    .font(.system(size: 25,weight: .regular, design: .default) )
+                            }
+                            )
+                            Button(action: {
+                                print("Holi2")
+                            }, label: {
+                                Image(systemName: "speaker.wave.2.fill")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width:25)
+                                    .padding(.horizontal,5)
+                            Text(  "Ejemplo").foregroundColor(.black).fontWeight(.semibold)
+                            }
+                            )
+                        }
+                    }
+                }
+
+                .padding()
+            }
         }
-        .padding()
     }
 }
 
