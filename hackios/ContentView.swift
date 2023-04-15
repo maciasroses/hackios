6f481b2bfa5470aaba6cd11b86ca1f030f9cc290
Macias Romero Jorge Humberto <a00573134@itesm.mx>
2023-04-15 15:44:34 -0600

    EspaniolView

--- hackios/ContentView.swift
+++ hackios/ContentView.swift
@@ -9,62 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack{
-            HStack {
-                VStack{
-                    Text("Title1")
-                    ForEach(0..<3, id:  \.self) { number in
-                        HStack{
-                            Button(action: {
-                                print("Holi")
-                            }, label: {
-                                Image(systemName: "speaker.wave.2.fill")
-                                    .font(.system(size: 25,weight: .regular, design: .default) )
-                            }
-                            )
-                            Button(action: {
-                                print("Holi2")
-                            }, label: {
-                                Image(systemName: "speaker.wave.2.fill")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width:25)
-                                    .padding(.horizontal,5)
-                            Text(  "Ejemplo").foregroundColor(.black).fontWeight(.semibold)
-                            }
-                            )
-                        }
-                    }
+        NavigationStack{
+            VStack{
+                NavigationLink{
+                    EspaniolView()
+                } label: {
+                    Text("Arroz")
                 }
-                .padding()
-                
-                VStack{
-                    Text("Title2")
-                    ForEach(0..<3, id:  \.self) { number in
-                        HStack{
-                            Button(action: {
-                                print("Holi")
-                            }, label: {
-                                Image(systemName: "speaker.wave.2.fill")
-                                    .font(.system(size: 25,weight: .regular, design: .default) )
-                            }
-                            )
-                            Button(action: {
-                                print("Holi2")
-                            }, label: {
-                                Image(systemName: "speaker.wave.2.fill")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width:25)
-                                    .padding(.horizontal,5)
-                            Text(  "Ejemplo").foregroundColor(.black).fontWeight(.semibold)
-                            }
-                            )
-                        }
-                    }
-                }
-
-                .padding()
             }
         }
     }
