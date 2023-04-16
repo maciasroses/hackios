19a4afd24b7b6b4faa98c2564931a2617fac3d54
Francisco Mestizo <a01731549@itesm.mx>
2023-04-15 18:10:52 -0600

    Cambios correctos a pantalla inicial

--- hackios/ContentView.swift
+++ hackios/ContentView.swift
@@ -9,13 +9,70 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack{
+            VStack{
+                HStack {
+                    Spacer()
+                    Button{
+                        //Acciones
+                    } label: {
+                        Image(systemName: "waveform.circle.fill")
+                            .foregroundColor(.orange)
+                            .font(.system(size: 72))
+                            .padding(.trailing, 24)
+                    }
+                }
+                Text("Intelia")
+                    .font(.system(size: 92, weight: .bold))
+                    .padding(.top, 90)
+                Text("Selecciona alguno de los botones para aprender más sobre ellos.")
+                    .foregroundColor(.gray)
+                    .font(.system(size: 32))
+                    .padding(.bottom, 120)
+                HStack{
+                    NavigationLink{
+                        //EspaniolView()
+                    } label: {
+                        Text("Español 📓")
+                            .font(.system(size: 32, weight: .semibold))
+                            .foregroundColor(.white)
+                            .padding(.horizontal, 90)
+                            .padding(.vertical, 26)
+                            .background(RoundedRectangle(cornerRadius: 20)
+                                .fill(.blue)
+                                .shadow(radius: 4))
+                    }
+                    .padding(.trailing, 75)
+                    
+                    NavigationLink{
+                        //MateView()
+                    } label: {
+                        Text("Matemáticas 🧮")
+                            .font(.system(size: 32, weight: .semibold))
+                            .foregroundColor(.white)
+                            .padding(.horizontal, 70)
+                            .padding(.vertical, 26)
+                            .background(RoundedRectangle(cornerRadius: 20)
+                                .fill(.blue)
+                                .shadow(radius: 4))
+                    }
+                }
+                .padding(.bottom, 40)
+                Spacer()
+                HStack{
+                    Spacer()
+                    Button{
+                        //Abrir popup para ver la informacion de la aplicacion y porque se hizo
+                    } label: {
+                        Image(systemName: "questionmark.square.fill")
+                            .foregroundColor(.black)
+                            .font(.system(size: 48))
+                            .padding(.leading, 24)
+                    }
+                    Spacer()
+                }
+            }
         }
-        .padding()
     }
 }
 
